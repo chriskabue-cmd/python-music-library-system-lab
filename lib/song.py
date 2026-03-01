ddba982fa0bcd7e455f7e8c4a873899c2934daec
Chris kabue <chris.kabue@student.moringaschool.com>
2026-03-01 21:39:25 +0300

    Update song.py

--- lib/song.py
+++ lib/song.py
@@ -17,26 +17,26 @@ class Song:
         Song.add_to_genre_count(genre)
         Song.add_to_artist_count(artist)
 
-    classmethod
+    @classmethod
     def add_song_to_count(cls):
         cls.count += 1
 
-    classmethod
+    @classmethod
     def add_to_genres(cls, genre):
         cls.genres.add(genre)
 
-    classmethod
+    @classmethod
     def add_to_artists(cls, artist):
         cls.artists.add(artist)
 
-    classmethod
+    @classmethod
     def add_to_genre_count(cls, genre):
         if genre in cls.genre_count:
             cls.genre_count[genre] += 1
         else:
             cls.genre_count[genre] = 1
 
-    classmethod
+    @classmethod
     def add_to_artist_count(cls, artist):
         if artist in cls.artist_count:
             cls.artist_count[artist] += 1
